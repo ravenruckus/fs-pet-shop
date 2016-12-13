6965763259b71248b9055126f5465dc7ce5055a3
ravenruckus <gyorihome@hotmail.com>
2016-12-12 16:16:30 -0800

    Add read index

--- pets.js
+++ pets.js
@@ -1,15 +1,39 @@
+/* eslint-disable no-console */
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 const petsPath = path.join(__dirname, 'pets.json');
 
-fs.readFile(petsPath, 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  }
+const node = path.basename(process.argv[0]);
+const file = path.basename(process.argv[1]);
+const cmd = process.argv[2];
+const pet = process.argv[3];
 
-  const pets = JSON.parse(data);
+if (cmd === 'read') {
+  fs.readFile(petsPath, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const pets = JSON.parse(data);
 
-  console.log(pets);
-})
+    if (pet) {
+      if (pet < 0 || pet > pets.length - 1) {
+        console.error(`Usage: ${node} ${file} read INDEX`);
+      }
+      else {
+        console.log(pets[pet]);
+      }
+    }
+    else {
+      console.log(pets);
+    }
+  });
+}
+else if (cmd === 'create') {
+  console.log('created');
+}
+else {
+  console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+  process.exit(2);
+}
